5d98c6e98ed6d8a582599bc3bc41573ff64f7ed1
hanzii <pwe1ewq@gmail.com>
2022-08-22 00:46:54 +0900

    홈 스크린 배너 만들기

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,10 +1,61 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import styled from "styled-components";
+import { getMovies, IGetMovieResult } from "../api";
+import { makeImgPath } from "../utils";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 25px;
+  width: 50%;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  const { data, isLoading } = useQuery<IGetMovieResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
   console.log(data, isLoading);
-  return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          {/* 어떠한 이유에서 data가 존재하지 않아 backdrop_path가 undefined라면 그냥 ""<-이걸 보내 */}
+          <Banner bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,26 @@
+import { Spring } from "framer-motion";
+import { NumberLiteralType } from "typescript";
+
 const API_KEY = "4a648eb924ccac5ff6d4e455936456fd";
 const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  overview: string;
+  poster_path: string;
+  title: string;
+}
+export interface IGetMovieResult {
+  datas: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_results: number;
+  total_pages: NumberLiteralType;
+}
 
 export function getMovies() {
   return fetch(
